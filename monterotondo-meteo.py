9619693784c352ec407f74011631a12a4b9a2dc2
faustobuscema <fausto.buscema@investech.it>
2026-05-13 18:15:00 +0200

    Update monterotondo-meteo.py
    Modifica Nome pagina

--- monterotondo-meteo.py
+++ monterotondo-meteo.py
@@ -10,7 +10,7 @@ from datetime import datetime
 # Configurazione pagina
 # ------------------------------
 st.set_page_config(
-    page_title="Meteo Italia 2000-2025 + Tempo Reale",
+    page_title="Monterotondo (RM) Meteo",
     page_icon="🌦️",
     layout="wide"
 )
